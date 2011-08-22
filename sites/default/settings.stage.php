f7317bfe2db7263e03477352b05cd50f3fcc8664
Elliott Foster <elliott@fourkitchens.com>
2011-08-22 11:23:03 -0500

    Generalized database connection info.

--- sites/default/settings.stage.php
+++ sites/default/settings.stage.php
@@ -1,3 +1,9 @@
 <?php
+
+/**
+ * Stage database settings.
+ */
 $databases['default']['default']['database'] = 'pf_d';
 $databases['default']['default']['username'] = 'pf_d';
+$databases['default']['default']['password'] = '1HmOSsZdOI';
+
