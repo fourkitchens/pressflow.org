0679b591ef8291b49afb6ed656ca729de1088184
Chris Ruppel <chris@fourkitchens.com>
2012-05-01 15:09:34 -0500

    Copying pflow theme so we can start fresh.

---
+++ sites/all/themes/pfo/template.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Implements theme_preprocess().
+ */
+function pfo_preprocess(&$vars) {
+ // kpr($vars);
+}
+
+/**
+ * Implements theme_preprocess_page().
+ */
+function pfo_preprocess_page(&$vars) {
+ // kpr($vars);
+}
