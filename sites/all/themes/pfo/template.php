c80573742cedb4e11e816d2a7aaaf34cc6ed2aeb
Chris Ruppel <chris@fourkitchens.com>
2012-05-07 17:08:09 -0500

    Adding the $download variable so the page--front.tpl stops complaining.

--- sites/all/themes/pfo/template.php
+++ sites/all/themes/pfo/template.php
@@ -4,12 +4,14 @@
  * Implements theme_preprocess().
  */
 function pfo_preprocess(&$vars) {
- // kpr($vars);
+  // kpr($vars);
 }
 
 /**
  * Implements theme_preprocess_page().
  */
 function pfo_preprocess_page(&$vars) {
- // kpr($vars);
+  // kpr($vars);
+
+  $vars['download'] = '<a href="###githubbb">Download!</a>'; // this URL will be (at least partially) built by Ian's github integration code.
 }
