19ad3d2cd65f106f3b96a74368c1413606573086
Chris Ruppel <chris@fourkitchens.com>
2012-05-08 00:49:18 -0500

    Unsetting <body> page titles and setting <head> title.

--- sites/all/themes/pfo/template.php
+++ sites/all/themes/pfo/template.php
@@ -11,7 +11,41 @@ function pfo_preprocess(&$vars) {
  * Implements theme_preprocess_page().
  */
 function pfo_preprocess_page(&$vars) {
-  // kpr($vars);
 
-  $vars['download'] = '<a href="###githubbb">Download!</a>'; // this URL will be (at least partially) built by Ian's github integration code.
+  // Special treatment for front page
+  if ($vars['is_front'] == TRUE) {
+
+    // Download link for latest PRessflow release.
+    // This URL will is built by Ian's github integration code.
+    $vars['download'] = '<a href="###githubbb" id="download">Download!</a>';
+
+    // Unset the page title
+    $vars['title'] = '';
+  }
+}
+
+/**
+ * Implements theme_preprocess_node().
+ */
+function pfo_preprocess_node(&$vars) {
+
+  // Special treatment for front page
+  if ($vars['is_front'] == TRUE) {
+
+    // Unset the page title
+    $vars['title'] = '';
+  }
+}
+
+/**
+ * Implements pfo_preprocess_html().
+ */
+function pfo_preprocess_html(&$vars) {
+
+  // Special treatment for front page
+  if ($vars['is_front'] == TRUE) {
+
+    // Set the <title> tag
+    $vars['head_title'] = 'Pressflow | Enhanced performance and scalability for Drupal';
+  }
 }
