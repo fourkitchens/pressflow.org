d4c23cb1b73596f4771c142c9eb5ccbf7fddc90d
Chris Ruppel <chris@fourkitchens.com>
2012-06-29 20:15:04 -0500

    #38 - initial pass at real-time filtering. hoorah!

--- sites/all/themes/pfo/js/script.js
+++ sites/all/themes/pfo/js/script.js
@@ -1,4 +1,71 @@
+/**
+ * Pressflow-specific logic
+ */
 (function ($) {
+
+/**
+ * Mega-awesome real-time search thingy!!
+ * Filters FAQs as you type
+ *
+ * @author
+ *   Chris Ruppel
+ */
+
+Drupal.behaviors.faqSearch = {
+  attach: function (context) {
+
+    // vars
+    var faqs = [];
+    var i = 0; // start at 1 to stay in sync with Views classes
+
+    // Fetch FAQs once to save cycles
+    $('.views-row').each(function(){
+
+      // collect data from markup
+      faqs[i] = {
+        q: $(this).find('.question span').html(),
+        a: $(this).find('.answer div').html(),
+        id: i++,
+      };
+
+    });
+
+    // insert search box
+    $('.page-faq .view-faq-view').prepend('<input id="magic-search" type="text" placeholder="type here to search">');
+
+    // Search code. Does simple searching on each key press,
+    // then shows/hides individual FAQs
+    $('#magic-search').keyup(function(){
+
+      // More vars
+      var val = $(this).val().toLowerCase(),
+          test = '';
+
+      // Loop through data and see which ones match
+      for(var x=0; x <= $(faqs).length; x++){
+
+        // Moar vars!!!
+        var thisFaq = faqs[x];
+        var question = '';
+        var answer = '';
+
+        // flatten
+        question = thisFaq.q.toString().toLowerCase();
+        answer = thisFaq.a.toString().toLowerCase();
+
+        // Filter this entry, then show/hide
+        if ((question.indexOf(val)) >= 0 || (answer.indexOf(val) >= 0)) {
+          $('.views-row-'+(x+1)).show('fast');
+        }
+        else {
+          $('.views-row-'+(x+1)).hide('fast');
+        }
+
+      }
+    });
+
+  }
+};
 
 /*
 
