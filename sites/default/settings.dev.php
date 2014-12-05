a62356b4d3dcc3e8a46ec66d8cdaabdfa27a7dfc
Ian Carrico <git@iancarrico.com>
2014-12-05 13:51:27 -0600

    Alter the settings to work with Pantheon better

--- sites/default/settings.dev.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conf['preprocess_css'] = 0;
-$conf['preprocess_js'] = 0;
--- sites/default/settings.live.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Live database settings.
- */
-$databases['default']['default']['database'] = 'pf';
-$databases['default']['default']['username'] = 'pf';
-$databases['default']['default']['password'] = '1HmOSsZdOI';
-
